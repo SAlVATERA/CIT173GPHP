33dbfacc8fb40a0a0b3094784acc38fe0f8999fa
unknown <CIT173B-9@S312.UC-BCF.EDU.PH>
2023-09-27 18:01:43 +0800

    helloworld

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<?php
+    echo "helloworld";
+    $txt = "you";
+    echo "<br> I love " .$txt. "!";
+    $x = 5;
+    $y = 9;
+    echo "<br>";
+    echo $x + $y;
+    echo "<br>-------";
+    function mytest()
+    {
+        $z = 50;
+        echo "<p>Variable z inside function is : $z</p>";
+    }
+    mytest();
+?>
