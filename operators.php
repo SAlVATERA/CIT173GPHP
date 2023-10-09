e7c4274e59c7372c2c88b894490518721d7028a9
SAlVATERA <jasonsalvatera24680@gmail.com>
2023-10-09 17:32:41 +0800

    more changes

---
+++ operators.php
@@ -0,0 +1,25 @@
+<?php
+$true_num = 3 + 0.14159;
+$true_str = "Tried and true"
+$true_array[49] = "An array element";
+$false_array = array();
+$false_null = NULL;
+$false_num = 999 - 999;
+$false_str = "";
+print ($true_num);
+print ($true_str);
+print ($true_array);
+print ($false_array);
+print ($false_null);
+print ($false_num);
+print ($false_str);
+$string_1 = "This is a string in double quotes";
+$string_2 = "This is a somewhat longer, singly quoted string";
+$string_39 = "This string has thirty-nine characters";
+$string_0 = "";
+$variable = "name";
+$literally = 'My $variable will not print!\\n';
+print($literally);
+$literally = "My $variable will print!\\n";
+print($literally);
+?>
