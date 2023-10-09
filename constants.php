a3fa932db34a9e92a24ec086dd0b1adc6e80683a
SAlVATERA <jasonsalvatera24680@gmail.com>
2023-10-09 17:22:35 +0800

    changes in school

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+$int_var = 12345;
+$another_int = -12345 + 12345;
+if (TRUE)
+print("<br>This will always print<br>");
+else
+print("This will never print<br>");
+$many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+print($many + $many_2 = $few);
+?>
