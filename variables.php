bc4f61ddfaea9878c499c9de125ac8fcdba110da
SAlVATERA <jasonsalvatera24680@gmail.com>
2023-10-09 17:18:44 +0800

    changes in school

--- variables.php
+++ variables.php
@@ -5,4 +5,24 @@
     $y = 8;
     echo $x + $y;
     echo "<br>testing 1";
+    if (3 == 2 + 1)
+    print("Good - I haven't totally lost my mind.<br>");
+    if (3 == 2 + 1)
+    {
+    print("Good - I haven't totally");
+    print("lost my mind.<br>");
+    }
+    $many = 2.2888800;
+    $many_2 = 2.2111200;
+    $few = $many + $many_2;
+    print($many + $many_2 = $few);
+    if (TRUE)
+    print("<br>This will always print<br>");
+    else
+    print("This will never print<br>");
+    $variable = "name";
+    $literally = 'My $variable will not print!\\n';
+    print($literally);
+    $literally = "My $variable will print!\\n";
+    print($literally);
 ?>
