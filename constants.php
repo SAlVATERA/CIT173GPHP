f04157b0a3b965ce11d666bdb22679e6fbc8ae2e
SAlVATERA <jasonsalvatera24680@gmail.com>
2023-10-09 17:36:32 +0800

    added comments in constants

--- constants.php
+++ constants.php
@@ -4,9 +4,10 @@ $another_int = -12345 + 12345;
 if (TRUE)
 print("<br>This will always print<br>");
 else
-print("This will never print<br>");
+print("This will never print<br>"); // some errors
 $many = 2.2888800;
 $many_2 = 2.2111200;
 $few = $many + $many_2;
 print($many + $many_2 = $few);
+
 ?>
